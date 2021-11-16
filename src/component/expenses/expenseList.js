d5edbef753ed588fba0f555245b8be8ae9180ad3
Ademola-Pamilerin <64588069+Ademola-Pamilerin@users.noreply.github.com>
2021-11-16 01:16:52 +0100

    Testing the App

--- src/component/expenses/expenseList.js
+++ src/component/expenses/expenseList.js
@@ -1,24 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
-import './expenseList.css';
-const ExpenseList=(props)=>{
-  console.log(props.remained)
-    if(props.remained.length === 0){
-        return (
-            <div className="empty-expense">No expense found</div>
-        )
-    }
-    return(
-      <div> {
-props.remained.map((element) => (
-    <ExpenseItem
-      key={element.id}
-      price={element.amount}
-      title={element.title}
-      date={element.date}
-    />
-  ))
-}
-  </div>
-)
-}
+import "./expenseList.css";
+const ExpenseList = (props) => {
+  console.log(props.remained);
+  if (props.remained.length === 0) {
+    return <div className="empty-expense">No expense found</div>;
+  }
+  return (
+    <div>
+      {" "}
+      {props.remained.map((elements) => (
+        <ExpenseItem
+          key={elements.id}
+          price={elements.amount}
+          title={elements.title}
+          date={elements.date}
+        />
+      ))}
+    </div>
+  );
+};
 export default ExpenseList;
